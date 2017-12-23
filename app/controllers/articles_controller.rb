50df2c6851bc53950733cff6eff9f561bf113aea
Mike Needle <MikeNeedle@gmail.com>
2017-12-23 19:01:36 +0000

    fixed display errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,24 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def index
+  end
+  
   def create
     #render plain: params[:article].inspect
-    article = Article.new(article_params) #to whitelist data for entry so it can be saved
-    article.save #save the data to the database
-    redirect_to articles_show_(@article) #after the article is saved, send the browser to show what is now in the article table
+    
+    @article = Article.new(article_params) #to whitelist data for entry so it can be saved
+    if @article.save #if saved ok
+      flash[:notice] = "Article was successfully saved" #outputs a notice to user all is well
+      redirect_to article_path(@article) #after the article is saved, send the browser to show what is now in the article table
+    else #issue with the save
+      render 'new' #display form for user to try again
+    end
+  end
+  
+  #shows the requested article
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
